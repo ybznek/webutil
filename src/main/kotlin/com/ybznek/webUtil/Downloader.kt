3cccb8b13cc98fb4a59e790f0fa685be575ab815
Zbynek Moravec <zbynek@ithvezda.cz>
2021-12-26 14:43:06 +0100

    rollbackToLatest

--- src/main/kotlin/com/ybznek/webUtil/Downloader.kt
+++ src/main/kotlin/com/ybznek/webUtil/Downloader.kt
@@ -1,9 +1,6 @@
 package com.ybznek.webUtil
 
-import org.jsoup.Jsoup
-import org.jsoup.helper.HttpConnection
 import java.io.File
-import java.net.URI
 
 object Downloader {
     fun <T> downloadIfMissing(f: File, write: (File) -> T): T? {
@@ -15,11 +12,5 @@ object Downloader {
 
     fun <T> downloadIfMissing(f: File, write: (File) -> T, read: (File) -> T): T {
         return downloadIfMissing(f, write) ?: read(f)
-    }
-
-    fun downloadAsText(url: String): String {
-        val connect = HttpConnection.connect(url)
-        connect.execute()
-        return connect.response().body()
     }
 }
